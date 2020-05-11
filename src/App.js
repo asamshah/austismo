cdb556fccbd464928c147c288c87569ff3319ec0
Jacanroman <jacanroman@gmail.com>
2020-05-11 23:01:54 +0100

    Added Bootstrap

--- src/App.js
+++ src/App.js
@@ -3,11 +3,14 @@ import './App.css';
 import FeelingsMenu from './components/FeelingsMenu/FeelingsMenu'
 import Schedule from './components/Schedule/Schedule'
 import AngryFeeling from './components/AngryFeeling/AngryFeeling'
+import Navbar from './components/Navbar'
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <h1>First Change</h1>
+      
       <FeelingsMenu />
       <Schedule />
       <AngryFeeling />

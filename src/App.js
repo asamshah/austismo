d6d741664f9cdefb3329049110296d5a55d42146
shona10 <shonamason10@gmail.com>
2020-05-11 21:30:29 +0100

    Basic UI structure

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import FeelingsMenu from './components/FeelingsMenu/FeelingsMenu'
+import Schedule from './components/Schedule/Schedule'
+import AngryFeeling from './components/AngryFeeling/AngryFeeling'
 
 function App() {
   return (
     <div className="App">
       <h1>First Change</h1>
+      <FeelingsMenu />
+      <Schedule />
+      <AngryFeeling />
+
+
     </div>
   );
 }

b0874e3aa0b23e76a833afe851238a3ca4bf41e6
Asam Shah <home@Asams-MacBook-Pro.local>
2020-05-12 01:03:42 +0100

    Added Homepage

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
 import React from 'react';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import FeelingsMenu from './components/FeelingsMenu/FeelingsMenu'
 import Schedule from './components/Schedule/Schedule'
-import AngryFeeling from './components/AngryFeeling/AngryFeeling'
+import mood from './images/mood.svg'
+import wait from './images/wait.svg'
+import logo from './images/Frame1.png'
 
 function App() {
   return (
-    <div className="App">
-      <h1>First Change</h1>
-      <FeelingsMenu />
-      <Schedule />
-      <AngryFeeling />
+    <div className="main">
+      <h1>Hey! Welcome to the Autism Scheduler App</h1>
+      <h2>Click on the images below to tell us how you're feeling today. Or how about updating your schedule.</h2>
 
-
+      <a href = {<FeelingsMenu />}><img src={mood} alt="mood" /></a> 
+      <a href = {<Schedule />}><img src={wait} alt="wait" /></a> 
     </div>
   );
 }

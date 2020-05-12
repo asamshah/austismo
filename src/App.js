76c6bb7a15d7a6328a78c95af35f76d45553ef44
Asam Shah <home@Asams-MacBook-Pro.local>
2020-05-12 01:19:30 +0100

    Added Nav

--- src/App.js
+++ src/App.js
@@ -3,12 +3,15 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import FeelingsMenu from './components/FeelingsMenu/FeelingsMenu'
 import Schedule from './components/Schedule/Schedule'
+import Navbar from './components/Navbar'
 import mood from './images/mood.svg'
 import wait from './images/wait.svg'
-import logo from './images/Frame1.png'
 
 function App() {
   return (
+    <nav>
+      <Navbar />
+    </nav>,
     <div className="main">
       <h1>Hey! Welcome to the Autism Scheduler App</h1>
       <h2>Click on the images below to tell us how you're feeling today. Or how about updating your schedule.</h2>

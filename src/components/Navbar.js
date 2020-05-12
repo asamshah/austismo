11e451e663ebb3b9a2e5743b2c4f8c29f49f0b28
Jacanroman <jacanroman@gmail.com>
2020-05-12 09:37:18 +0100

    NavBar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,11 @@
 import React from 'react';
 
-
 function Navbar() {
     return (
       <div class="App">
   
             <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-              <a className="navbar-brand" href="#">Autism APP</a>
+              <a className="navbar-brand" href="#"> Autism App</a>
               <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
               </button>
@@ -16,10 +15,10 @@ function Navbar() {
                     <a className="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
                   </li>
                   <li className="nav-item">
-                    <a className="nav-link" href="#">Schedule</a>
+                    <a className="nav-link" href="<Schedule />">Schedule</a>
                   </li>
                   <li className="nav-item">
-                    <a className="nav-link" href="#">MyFeelings</a>
+                    <a className="nav-link" href="<FeelingsMenu />">Mood</a>
                   </li>
                   
                 </ul>
@@ -30,4 +29,5 @@ function Navbar() {
     );
   }
   
+
   export default Navbar;

6bfe5645ba5a4346219342105f585573e99f8eb3
Asam Shah <home@Asams-MacBook-Pro.local>
2020-05-12 11:17:58 +0100

    Added H3 Tag

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import FeelingsMenu from './components/FeelingsMenu/FeelingsMenu'
 import Schedule from './components/Schedule/Schedule'
+import FeelingsMenu from './components/FeelingsMenu/FeelingsMenu'
 import mood from './images/mood.svg'
 import wait from './images/wait.svg'
 
@@ -8,7 +8,8 @@ function Home() {
     return (
         <div className="main">
             <h1>Hey! Welcome to the Autism Scheduler App</h1>
-            <h2>Click on the images below to tell us how you're feeling today. Or how about updating your schedule.</h2>
+            <h2>Click on the images below to tell us how you're feeling today.</h2> 
+            <h3>Or how about updating your schedule.</h3>
 
             <a href = {<FeelingsMenu />}><img src={mood} alt="mood" /></a> 
             <a href = {<Schedule />}><img src={wait} alt="wait" /></a> 

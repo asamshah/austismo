eab6f890bad8a2e2eeb566ffd87efe5cba14888f
Jacanroman <jacanroman@gmail.com>
2020-05-12 10:08:25 +0100

    Test

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import './App.css';
-<<<<<<< HEAD
 import FeelingsMenu from './components/FeelingsMenu/FeelingsMenu'
 import Schedule from './components/Schedule/Schedule'
 import AngryFeeling from './components/AngryFeeling/AngryFeeling'
@@ -17,18 +16,6 @@ function App() {
       <AngryFeeling />
 
 
-=======
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Home from './components/Home/Home';
-import Navbar from './components/Navbar';
-
-
-function App() {
-  return (
-    <div>
-      <Navbar />
-      <Home />
->>>>>>> 6c7d9a739a0004f6669f8c3f2948741d071e367e
     </div>
   )
 }

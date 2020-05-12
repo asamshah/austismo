6c7d9a739a0004f6669f8c3f2948741d071e367e
Asam Shah <home@Asams-MacBook-Pro.local>
2020-05-12 01:29:28 +0100

    Added Home component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
 import React from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import FeelingsMenu from './components/FeelingsMenu/FeelingsMenu'
-import Schedule from './components/Schedule/Schedule'
-import Navbar from './components/Navbar'
-import mood from './images/mood.svg'
-import wait from './images/wait.svg'
+import Home from './components/Home/Home';
+import Navbar from './components/Navbar';
+
 
 function App() {
   return (
-    <nav>
+    <div>
       <Navbar />
-    </nav>,
-    <div className="main">
-      <h1>Hey! Welcome to the Autism Scheduler App</h1>
-      <h2>Click on the images below to tell us how you're feeling today. Or how about updating your schedule.</h2>
-
-      <a href = {<FeelingsMenu />}><img src={mood} alt="mood" /></a> 
-      <a href = {<Schedule />}><img src={wait} alt="wait" /></a> 
+      <Home />
     </div>
-  );
+  )
 }
 
 export default App;
